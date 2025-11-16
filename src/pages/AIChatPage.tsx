cf23cea619c6f6c751ea57b428a50cf9003bbf1f
[dyad] <theekiller028@gmail.com>
2025-11-16 01:23:23 -0300

    [dyad] Ajustando o layout da página do assistente virtual para fixar a barra de entrada - wrote 1 file(s)

--- src/pages/AIChatPage.tsx
+++ src/pages/AIChatPage.tsx
@@ -2,7 +2,7 @@ import { AIChat } from "@/components/FAQ/AIChat";
 
 const AIChatPage = () => {
   return (
-    <div className="container mx-auto px-4 md:pl-8 flex flex-col flex-1 py-8"> {/* Alterado h-full para flex-1 */}
+    <div className="container mx-auto px-4 md:pl-8 flex flex-col flex-1 h-full"> {/* Adicionado h-full e removido py-8 */}
       {/* O título 'Assistente Virtual' foi movido para dentro do componente AIChat */}
       <AIChat />
     </div>
